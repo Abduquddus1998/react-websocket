ee0d45bc20b978fb74b8be42a11ef1a89e5bdc0d
Azizov <azizov.a@ad.loc>
2019-06-24 18:23:43 +0500

    websocket app

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+
+const port = process.env.PORT || 4001;
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', socket => {
+    console.log("User connected");
+
+    socket.on('change color', color => {
+        io.sockets.emit('change color', color);
+        console.log('Change Color to:', color)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected");
+    })
+})
+
+server.listen(port, () => console.log("Listening on port"+ "  " +port));
